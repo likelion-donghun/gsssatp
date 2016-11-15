60a892637c96fca895a372a58cf2376e8d1c75c3
likelion_donghun <136shin@gmail.com>
2016-11-15 03:50:38 +0000

    eatery ranking

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @eatery = Eatery.all
+    @ranking = Eatery.joins(:likes).group("likes.eatery_id").order("count(likes.eatery_id) desc")
+    
   end
 end
